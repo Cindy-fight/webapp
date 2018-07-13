757b9f454b1cc2b9ec7e745bc24ae8419988f838
Cindy-fight <ting.wang@maimob.cn>
2018-07-13 16:11:51 +0800

    更新数据库密码

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Config():
     Base = declarative_base()
-    engine = create_engine('mysql+mysqlconnector://root:Mtt2017@localhost:3306/webapp')
+    engine = create_engine('mysql+mysqlconnector://root:Wtt2018@localhost:3306/webapp')
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
